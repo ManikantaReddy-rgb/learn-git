2af9c682a7fbe810efe4802bddbf3099a3cc6f8b
Manikanta Reddy <manikantareddy9848@gmail.com>
2022-12-05 12:53:08 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,21 @@
+# #camel casing
+# noOfCars = 0
+# noOfBikes = 0
+# totalpayment = 0
+# noOfCars = int(input("no. of cars"))
+# noOfBikes = int(input("no. of bikes"))
+# totalpayment = noOfBikes*20+noOfCars*40
+# print("total payment is:", totalpayment)
+
+
+Product1 = 40
+Product2 = 50
+Product3 = 60
+quantity1=int(input("enter the quantity of product1"))
+quantity2=int(input("enter the quantity of product2"))
+quantity3=int(input("enter the quantity of product3"))
+p1amount=Product1*quantity1
+p2amount=Product2*quantity2
+p3amount=Product3*quantity3
+totalamount=p1amount+p2amount+p3amount
+print("the total amount you need to pay is:",totalamount)
